21013227c77ceb990fd3ba72175c4466413b02ba
Olena1986 <125265067+Olena1986@users.noreply.github.com>
2023-04-17 11:33:22 +0200

    task9-10

--- src/js/task-10.js
+++ src/js/task-10.js
@@ -3,3 +3,32 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+function createBoxes(amount) {
+    const boxesEl = document.querySelector('#boxes');
+    let size = 30;
+    for (let i = 0; i < amount; i++) {
+      const boxEl = document.createElement('div');
+      boxEl.style.backgroundColor = getRandomHexColor();
+      boxEl.style.width = `${size}px`;
+      boxEl.style.height = `${size}px`;
+      boxesEl.appendChild(boxEl);
+      size += 10;
+    }
+  }
+
+  function destroyBoxes() {
+    const boxesEl = document.querySelector('#boxes');
+    boxesEl.innerHTML = '';
+  }
+
+  const createBtn = document.querySelector('[data-create]');
+  const destroyBtn = document.querySelector('[data-destroy]');
+  const input = document.querySelector('input[type="number"]');
+
+  createBtn.addEventListener('click', () => {
+    createBoxes(input.value);
+  });
+
+  destroyBtn.addEventListener('click', () => {
+    destroyBoxes();
+  });
