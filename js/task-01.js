98e44d537f76ac7591899ae5b83f93d88b5ea53b
Olena1986 <125265067+Olena1986@users.noreply.github.com>
2023-04-19 15:01:29 +0200

    new 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,19 @@
-const itemEl = document.querySelectorAll('#categories .item');
+// const itemEl = document.querySelectorAll('#categories .item');
 
-console.log(`Number of categories: ${itemEl.length}`);
+// console.log(`Number of categories: ${itemEl.length}`);
 
-itemEl.forEach(item => {
-  const categoryName = item.querySelector('h2').textContent;
-  const elementCount = item.querySelectorAll('li').length;
-  console.log(`Category: ${categoryName}\nElements: ${elementCount}`);
+// itemEl.forEach(item => {
+//   const categoryName = item.querySelector('h2').textContent;
+//   const elementCount = item.querySelectorAll('li').length;
+//   console.log(`Category: ${categoryName}\nElements: ${elementCount}`);
+// });
+
+
+const itemEl = document.querySelectorAll('.item');
+console.log('Number of categories:', itemEl.length);
+
+itemEl.forEach(category => {
+    console.log('Category:', category.firstElementChild.textContent);
+    console.log('Elements:', category.lastElementChild.children.length);
 });
-
-
-
 
